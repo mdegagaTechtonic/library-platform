d445428fe7236464aa5ea92c339d95a32f2aeb1d
Merry Degaga <merry.degaga@techtonic.com>
2018-10-02 15:01:42 -0600

    initial commit

---
+++ js/book.js
@@ -0,0 +1,9 @@
+//constructor 
+function Book(title, author, numPages, date){
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.pubDate = date;
+}
+//methods
+Book.prototype.editBook = function(obj){};
---
+++ js/engine.js
@@ -0,0 +1,15 @@
+//construtor
+function Library() {
+  this.bookShelf = new Array();
+};
+
+//method
+Library.prototype.addBook = function(book){};
+Library.prototype.removeBookByTitle = function(title) {};
+
+
+//Will create the instance of our library
+document.addEventListener("DOMContentLoaded",function(e){
+  //will access all the libaray methods
+  window.gLibrary = new Library();
+});
