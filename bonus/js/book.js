58794ba2b239034cd9fada2f80d092fba369c74b
Merry Degaga <merry.degaga@techtonic.com>
2018-10-04 11:40:34 -0600

    created and tested singleton

---
+++ bonus/js/book.js
@@ -0,0 +1,49 @@
+/**
+ * @file The Book class
+ * @author Merry Degaga
+ * @version 10.3.2018
+ */
+
+/**
+* Constructor will create a book from parameters
+* @param {String} title
+* @param {String} author
+* @param {number} numPages is the number of pages
+* @param {Date} pubDate is the published date
+*/
+function Book(title, author, numPages, pubDate) {
+  //Can error check to make sure all the fields were provided and they are not null
+  this.title = title.trim();
+  this.author = author.trim();
+  this.numPages = numPages;
+  this.pubDate = pubDate;
+}
+
+//methods
+/**
+* Takes an object that can have some or all of the fields {title,author,numPages} and edits the book using these fields
+* @param {Object} book
+* @return {Book} the edited book object
+*/
+Book.prototype.editBook = function(oBook){
+  //how about undefined properties of oBook?
+  if(oBook.title !== null) {
+    this.title = oBook.title;
+  }
+  if(oBook.author !== null) {
+    this.author = oBook.author;
+  }
+  if(oBook.numPages !== null) {
+    this.numPages = oBook.numPages;
+  }
+  return this; 
+};
+
+
+// //testing
+// var j = new Book('ba', 'dan', 56, 1889);
+// console.log(j);
+//
+// if(!j===undefined) {
+//   console.log('true');
+// }
